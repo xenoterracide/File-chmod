caaa9a0c0709577b0c9b34ef2ae5962b3d243aef
Caleb Cushing <xenoterracide@gmail.com>
2015-02-14 12:30:47 -0600

    GH #5 enable deprecation warning
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/File/chmod.pm
+++ lib/File/chmod.pm
@@ -59,12 +59,12 @@ sub getchmod {
 sub symchmod {
   my $mode = shift;
 
-#  warnings::warnif 'deprecated', '$UMASK being true is deprecated'
-#    . ' it will be false by default in the future. This change'
-#    . ' is being made because this not the behavior of the unix command'
-#    . ' `chmod`. This warning can be disabled by putting explicitly'
-#    . ' setting $File::chmod::UMASK to false or any non 2 true value'
-#    if $UMASK == 2;
+warnings::warnif 'deprecated', '$UMASK being true is deprecated'
+  . ' it will be false by default in the future. This change'
+  . ' is being made because this not the behavior of the unix command'
+  . ' `chmod`. This warning can be disabled by putting explicitly'
+  . ' setting $File::chmod::UMASK to false or any non 2 true value'
+  if $UMASK == 2;
 
   my @return = getsymchmod($mode,@_);
   my $ret = 0;
